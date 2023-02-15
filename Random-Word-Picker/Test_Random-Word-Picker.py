c709b7372d2c195f831baccfcad2d00e96b60fd8
Wilson Smith <donatosuozziwork@gmail.com>
2023-02-15 17:03:20 +0100

    Code Polishing

--- Random-Word-Picker/Test_Random-Word-Picker.py
+++ Random-Word-Picker/Test_Random-Word-Picker.py
@@ -1,28 +1,35 @@
 # Libraries are here
 from tkinter import *
+import random
+import requests
+import os
+
+def DictionaryAccess():
+    # Open the dictionary file
+    with open('C:\\Users\\utente\\Desktop\\Random-Word-Generator\\dictionary.txt', 'r') as f:
+        # Read all the lines from the file
+        lines = f.readlines()
+    return lines
+        
+def WordPicker(lines):
+    # Choose a random line from the file
+    random_line = random.choice(lines)
+
+    # Split the line by whitespace to get the word
+    random_word = random_line.split()[0]
+    return random_word
+
+def Url(word):
+    # Creation of the link
+    url = 'https://www.urbandictionary.com/define.php?term=' + word + '/'
+    return url
 
 def button():
-    import random
-    import requests
-    import os
-
     while True:
+        lines = DictionaryAccess()
+        word = WordPicker(lines)
 
-        import random
-
-        # Open the dictionary file
-        with open('C:\\Users\\utente\\Desktop\\Random-Word-Generator\\dictionary.txt', 'r') as f:
-            # Read all the lines from the file
-            lines = f.readlines()
-
-        # Choose a random line from the file
-        random_line = random.choice(lines)
-
-        # Split the line by whitespace to get the word
-        random_word = random_line.split()[0]
-
-        # Creation of the link
-        url = 'https://www.urbandictionary.com/define.php?term=' + random_word + '/'
+        url = Url(word)
         r = requests.get(url)
 
         # Launching Chrome if there is a real hentai
